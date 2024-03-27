384ff72c55923475c851d26ce0c64baedeefc6f6
e <minnki19@gmail.com>
2024-03-27 15:49:49 +0900

    go

---
+++ src/test/kotlin/com/daelim/springtest/SpringTestApplicationTests.kt
@@ -0,0 +1,15 @@
+package com.daelim.springtest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+
+
+	}
+
+}
